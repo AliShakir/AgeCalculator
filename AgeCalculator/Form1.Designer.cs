b2a23d32f4667fb8fb61e7681b693ac79538a98d
Shakir Ali <shakirali.mit@gmail.com>
2019-11-15 20:29:03 +0500

    Age calculator without key press event

---
+++ AgeCalculator/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace AgeCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.dtpDob = new System.Windows.Forms.DateTimePicker();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblDays = new System.Windows.Forms.Label();
+            this.lblMonths = new System.Windows.Forms.Label();
+            this.lblYears = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(12, 54);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(221, 26);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // dtpDob
+            // 
+            this.dtpDob.CalendarFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpDob.CustomFormat = "dd/MM/yyy";
+            this.dtpDob.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpDob.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpDob.Location = new System.Drawing.Point(12, 21);
+            this.dtpDob.Name = "dtpDob";
+            this.dtpDob.Size = new System.Drawing.Size(221, 26);
+            this.dtpDob.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblDays);
+            this.groupBox1.Controls.Add(this.lblMonths);
+            this.groupBox1.Controls.Add(this.lblYears);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 86);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(221, 197);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Calculated Age";
+            // 
+            // lblDays
+            // 
+            this.lblDays.AutoSize = true;
+            this.lblDays.ForeColor = System.Drawing.Color.Red;
+            this.lblDays.Location = new System.Drawing.Point(32, 144);
+            this.lblDays.Name = "lblDays";
+            this.lblDays.Size = new System.Drawing.Size(83, 21);
+            this.lblDays.TabIndex = 2;
+            this.lblDays.Text = "0 Days old";
+            // 
+            // lblMonths
+            // 
+            this.lblMonths.AutoSize = true;
+            this.lblMonths.ForeColor = System.Drawing.Color.Red;
+            this.lblMonths.Location = new System.Drawing.Point(32, 96);
+            this.lblMonths.Name = "lblMonths";
+            this.lblMonths.Size = new System.Drawing.Size(76, 21);
+            this.lblMonths.TabIndex = 1;
+            this.lblMonths.Text = "0 Months";
+            // 
+            // lblYears
+            // 
+            this.lblYears.AutoSize = true;
+            this.lblYears.ForeColor = System.Drawing.Color.Red;
+            this.lblYears.Location = new System.Drawing.Point(32, 48);
+            this.lblYears.Name = "lblYears";
+            this.lblYears.Size = new System.Drawing.Size(60, 21);
+            this.lblYears.TabIndex = 0;
+            this.lblYears.Text = "0 Years";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(248, 291);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dtpDob);
+            this.Controls.Add(this.btnCalculate);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Age Calculator";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.DateTimePicker dtpDob;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblYears;
+        private System.Windows.Forms.Label lblDays;
+        private System.Windows.Forms.Label lblMonths;
+    }
+}
+
---
+++ AgeCalculator/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AgeCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            Display();
+        }
+        public void Display()
+        {
+            DateTime date = DateTime.Now;
+           // DateTime BirthDate = new DateTime(1985, 2, 11, 0, 0, 0);
+            TimeSpan diffResult = date.Subtract(dtpDob.Value);
+            string Months = ((diffResult.Days) % 365).ToString();
+            string RemainingMonths = (Convert.ToInt32(Months) / 31).ToString();
+            string RemainginYears = ((diffResult.Days) / 365).ToString();
+            string RemainingDays = (Convert.ToInt32(Months) % 31).ToString();
+            lblYears.Text = RemainginYears.ToString()+" Years";
+            lblMonths.Text = RemainingMonths.ToString() + " Months";
+            lblDays.Text = RemainingDays.ToString() + " Days old";
+            //MessageBox.Show(RemainginYears + "-" + RemainingMonths + "-" + RemainingDays);
+        }
+    }
+}
